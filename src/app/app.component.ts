bbf70909d86f69050b7907a5a0aab8c4a2dee547
Szabó Zoltán <szaboz@inf.u-szeged.hu>
2019-04-24 14:39:28 +0200

    first animation working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
 import { Component } from '@angular/core';
+import { trigger, state, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('makeMoreVisible', [
+      state('normal', style({
+        backgroundColor: 'white',
+        fontSize: "14px",
+        color: "black"
+      })),
+      state('visible', style({
+        backgroundColor: 'grey',
+        fontSize: '24px',
+        color: 'red'
+      })),
+      transition('normal => visible', [
+        animate('3s')
+      ]),
+      transition('visible => normal', [
+        animate('1s')
+      ])
+    ])
+  ]
 })
 export class AppComponent {
+  visibleText = false;
+  
   title = 'router2';
+
+  toggleVisible() {
+    this.visibleText = !this.visibleText;
+  }
 }
